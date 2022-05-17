7cffd5e6d845cc0962f27a76215202030d6948d7
Janaka-Steph <rochestephane@yahoo.fr>
2022-05-17 16:52:14 +0100

    Ecclib

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,17 @@
-module.exports = function override(config, env) {
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+    config.module.rules = config.module.rules.map((rule) => {
+        if (rule.oneOf instanceof Array) {
+            return {
+                ...rule,
+                oneOf: [{test: /\.wasm$/, type: 'asset/inline'}, ...rule.oneOf],
+            };
+        }
+        return rule;
+    });
+
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, {
         buffer: require.resolve('buffer'),
@@ -6,8 +19,22 @@ module.exports = function override(config, env) {
     });
     config.resolve.fallback = fallback;
 
+    config.resolve.alias = {
+        './wasm_loader.js': path.resolve(__dirname, 'src/ecclib.ts'),
+        './wasm_loader.browser.js': path.resolve(__dirname, 'src/ecclib.ts'),
+        'tiny-secp256k1-lib': path.resolve(__dirname, 'node_modules/tiny-secp256k1/lib'),
+    };
+
+    config.resolve.extensions = ['.tsx', '.ts', '.js', '.wasm'];
+
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ]);
+
     config.experiments = {
-        asyncWebAssembly: true,
+        topLevelAwait: true,
     };
     return config;
-}
+};
